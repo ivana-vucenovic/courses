3ac1e7a038c55f03703431d6372dda53f77b5159
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-12-27 15:20:59 -0500

    working on new_course

--- courses_app/views.py
+++ courses_app/views.py
@@ -3,7 +3,26 @@ from django.contrib import messages
 from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "courses": Course.objects.all()
+        }
+    return render(request, 'index.html', context)
 
 def new_course(request):
-    pass
+    errors = Course.objects.course_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+    else:
+        course = Course.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description']
+        )
+        # desc = Description.objects.create(content=request.POST['desc'])
+        # course.description = desc
+        course.save()
+
+    return redirect("/")
+
+
+
